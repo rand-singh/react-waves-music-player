b880d323341683f869c6c25ee680c6a94867bf3b
Randeep Singh <rand-singh@users.noreply.github.com>
2020-12-29 15:28:49 +0000

    Mobile styling and extra animations

--- src/App.js
+++ src/App.js
@@ -46,7 +46,7 @@ function App() {
   }
 
   return (
-    <div className="app">
+    <div className={`app ${libraryStatus ? 'library-active' : ''}`}>
       <Nav libraryStatus={libraryStatus} setLibraryStatus={setLibraryStatus} />
       <Song currentSong={currentSong} />
 
